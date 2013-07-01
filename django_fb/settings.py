83a195014701fc759fb711bb01c39c3b0d841703
Bosco Mutunga <boscomutunga@gmail.com>
2013-07-01 20:46:11 +0300

    update to heroku db settings

--- django_fb/settings.py
+++ django_fb/settings.py
@@ -180,6 +180,6 @@ import dj_database_url
 import os
 if os.getcwd() == '/app':
     DATABASES['default'] = dj_database_url.config(default='postgres://localhost')
-
+    STATIC_ROOT = root("static")
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
